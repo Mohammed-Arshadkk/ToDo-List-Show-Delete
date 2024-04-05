4446a3954e612801f2debab30d55ebb3410bd640
Mohammed Arshad <mohammedarshad62820@gmail.com>
2024-04-05 10:07:45 +0530

    ToDo list updated

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [toDos, setToDos] = useState([]);
+  const [toDo, setToDo] = useState("");
+
+  const removeToDo = (id) => {
+    setToDos(toDos.filter((todo) => todo.id != id));
+  };
+
+  return (
+    <div>
+      <div className="app">
+        <div className="mainHeading">
+          <h1>ToDo List</h1>
+        </div>
+        <div className="subHeading">
+          <br />
+          <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+        </div>
+        <div className="input">
+          <input
+            value={toDo}
+            onChange={(e) => setToDo(e.target.value)}
+            type="text"
+            placeholder="🖊️ Add item..."
+          />
+          <i
+            onClick={() =>
+              setToDos([
+                ...toDos,
+                { id: Date.now(), text: toDo, status: false },
+              ])
+            }
+            className="fas fa-plus"
+          ></i>
+        </div>
+        <div className="todos">
+          {toDos.map((obj) => {
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <div className="todo">
+                <div className="left">
+                  <input
+                    onChange={(e) => {
+                      console.log(e.target.checked);
+                      console.log(obj);
+                      setToDos(
+                        toDos.filter((obj2) => {
+                          if (obj2.id === obj.id) {
+                            obj2.status = e.target.checked;
+                          }
+                          return obj2;
+                        })
+                      );
+                    }}
+                    value={obj.status}
+                    type="checkbox"
+                    name=""
+                    id=""
+                  />
+                  <p>{obj.text}</p>
+                </div>
+                <div className="right">
+                  <i
+                    onClick={() => removeToDo(obj.id)}
+                    className="fas fa-times"
+                  ></i>
+                </div>
+              </div>
+            );
+          })}
+          {toDos.map((obj) => {
+            if (obj.status) {
+              return <h1>{obj.text}</h1>;
+            }
+            return null;
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
